20d59c65107235ee8eb8676168bf3e67952041df
elysiumcode <98084447+elysiumcode@users.noreply.github.com>
2022-02-11 21:49:10 +0300

    Add files via upload

---
+++ My First Project/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1334155.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 68441147, 68441148, 68441149, 68441150 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/68441171/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/68441172/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/68441170/1/ammo.js', 'preload' : true},
+];
